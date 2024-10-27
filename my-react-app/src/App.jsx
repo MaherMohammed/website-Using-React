fb9b69e90cecbce01c7b6df3e25ee8e2568a77e4
MaherMohammed <maher.mohammed10.97@gmail.com>
2024-10-27 22:46:08 +0200

    adding props in student component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,13 @@
-import Button from "./Components/Button/Button"
+import Student from "./Components/Student"
+
 
 function App() {
   return(
     <>
-      <Button/>
+      <Student name="Maher" age={30} isStudent={true}/>
+      <Student name="Patrick" age={42} isStudent={false}/>
+      <Student name="Sara" age={50} isStudent={false}/>
+      <Student name="Manar" age={27} isStudent={true}/>
     </>
   )
 }
